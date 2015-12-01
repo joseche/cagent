be50b358fe9fe2b8914a9edefd5170b0d07b305e
josebv <jhidalgo@beenverified.com>
2015-11-30 18:13:48 -0600

    pusher sending loadavgs, dirty and repetitive code ! :(

--- src/main/main.go
+++ src/main/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"collector"
 	"misc"
+	"pusher"
 )
 
 func main() {
@@ -10,6 +11,9 @@ func main() {
 
 	collector_done := make(chan bool)
 	go collector.Collector_routine(collector_done)
+	
+	pusher_done := make(chan bool)
+	go pusher.Pusher_routine(pusher_done)
 
 	<-collector_done
 }
